47ff426de344321039f8699075cbbb7919b4d376
Wanlin Li <wanlinli2018@gmail.com>
2022-03-19 16:51:10 -0400

    ECA

---
+++ insert_part2.sql
@@ -0,0 +1,3 @@
+/*-----------------ECA-------8 departments * 10 permits * 5 = 400 rows ------------------*/
+/* the dataset focus on ECA of 8 departments (4 hospital: IdEstablishment == 23,24,29,54 ; 4 medical clinics: IdEstablishment == 25,39,46,56)
+In each department, every permit have 5 ECA */
