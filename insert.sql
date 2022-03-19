90711433758178d6196a5fc203c0f7794af846e0
Wanlin Li <wanlinli2018@gmail.com>
2022-03-19 01:27:32 -0400

    try to add

---
+++ insert.sql
@@ -0,0 +1,46 @@
+INSERT INTO "ECA" VALUES(1,1);
+/*----------------UO-----------------*/
+insert into "UO" ("Id", "Name") values (1, 'Dalarna');
+insert into "UO" ("Id", "Name") values (2, 'New Brunswick');
+insert into "UO" ("Id", "Name") values (3, 'New Brunswick');
+insert into "UO" ("Id", "Name") values (4, 'Ohio');
+insert into "UO" ("Id", "Name") values (5, 'Ilha de São Miguel');
+insert into "UO" ("Id", "Name") values (6, 'Ohio');
+insert into "UO" ("Id", "Name") values (7, 'Jalisco');
+insert into "UO" ("Id", "Name") values (8, 'Jalisco');
+insert into "UO" ("Id", "Name") values (9, 'Ilha de São Miguel');
+insert into "UO" ("Id", "Name") values (10, 'Ilha de São Miguel');
+insert into "UO" ("Id", "Name") values (11, 'Illinois');
+insert into "UO" ("Id", "Name") values (12, 'Dalarna');
+insert into "UO" ("Id", "Name") values (13, 'La Rioja');
+insert into "UO" ("Id", "Name") values (14, 'New Brunswick');
+insert into "UO" ("Id", "Name") values (15, 'Ilha de São Miguel');
+insert into "UO" ("Id", "Name") values (16, 'Dalarna');
+insert into "UO" ("Id", "Name") values (17, 'Illinois');
+insert into "UO" ("Id", "Name") values (18, 'Dalarna');
+insert into "UO" ("Id", "Name") values (19, 'Jalisco');
+insert into "UO" ("Id", "Name") values (20, 'Ilha de São Miguel');
+
+select * from "UO";
+
+/*-----------Region-----------------*/
+insert into "Region" ("Id", "Name") values (1, 'Jalisco');
+insert into "Region" ("Id", "Name") values (2, 'New Brunswick');
+insert into "Region" ("Id", "Name") values (3, 'Ilha de São Miguel');
+insert into "Region" ("Id", "Name") values (4, 'Dalarna');
+insert into "Region" ("Id", "Name") values (5, 'Baden-Württemberg');
+insert into "Region" ("Id", "Name") values (6, 'Jalisco');
+insert into "Region" ("Id", "Name") values (7, 'Galicia');
+insert into "Region" ("Id", "Name") values (8, 'Ohio');
+insert into "Region" ("Id", "Name") values (9, 'Jalisco');
+insert into "Region" ("Id", "Name") values (10, 'Jalisco');
+insert into "Region" ("Id", "Name") values (11, 'New Brunswick');
+insert into "Region" ("Id", "Name") values (12, 'Galicia');
+insert into "Region" ("Id", "Name") values (13, 'Ilha de São Miguel');
+insert into "Region" ("Id", "Name") values (14, 'Buskerud');
+insert into "Region" ("Id", "Name") values (15, 'Ohio');
+insert into "Region" ("Id", "Name") values (16, 'Faro');
+insert into "Region" ("Id", "Name") values (17, 'Illinois');
+insert into "Region" ("Id", "Name") values (18, 'Illinois');
+insert into "Region" ("Id", "Name") values (19, 'New Brunswick');
+insert into "Region" ("Id", "Name") values (20, 'Faro');
