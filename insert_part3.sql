06bc810d9dd041185130815091948e9cab8b8537
Wanlin Li <wanlinli2018@gmail.com>
2022-03-19 19:00:52 -0400

    Activity

--- insert_part3.sql
+++ insert_part3.sql
@@ -1,8 +1,17 @@
 /*------------Activity----------------------*/
 /* the dataset focus on activities of two departments: gynaecology and neurology
-Activity list:
+Activity list: injections, diagnoses, surgeries, blood tests, removal of stitches, follow-up consultations, physical rehabilitation training, psychological consultation, academic seminar, experiments
 */
-
+insert into "Activity" ("Id", "Name", "Description") values (1, 'injections', 'This is a description waiting to be added');
+insert into "Activity" ("Id", "Name", "Description") values (2, 'diagnoses', 'This is a description waiting to be added');
+insert into "Activity" ("Id", "Name", "Description") values (3, 'surgeries', 'This is a description waiting to be added');
+insert into "Activity" ("Id", "Name", "Description") values (4, 'blood tests', 'This is a description waiting to be added');
+insert into "Activity" ("Id", "Name", "Description") values (5, 'removal of stitches', 'This is a description waiting to be added');
+insert into "Activity" ("Id", "Name", "Description") values (6, 'follow-up consultations', 'This is a description waiting to be added');
+insert into "Activity" ("Id", "Name", "Description") values (7, 'physical rehabilitation training', 'This is a description waiting to be added');
+insert into "Activity" ("Id", "Name", "Description") values (8, 'psychological consultation', 'This is a description waiting to be added');
+insert into "Activity" ("Id", "Name", "Description") values (9, 'academic seminar', 'This is a description waiting to be added');
+insert into "Activity" ("Id", "Name", "Description") values (10, 'experiments', 'This is a description waiting to be added');
 
 
 
