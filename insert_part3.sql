af0ea2eb619dd1a9351acd0924967b3530da1477
Wanlin Li <wanlinli2018@gmail.com>
2022-03-19 20:43:04 -0400

    programme

--- insert_part3.sql
+++ insert_part3.sql
@@ -13,14 +13,23 @@ insert into "Activity" ("Id", "Name", "Description") values (8, 'psychological c
 insert into "Activity" ("Id", "Name", "Description") values (9, 'academic seminar', 'This is a description waiting to be added');
 insert into "Activity" ("Id", "Name", "Description") values (10, 'experiments', 'This is a description waiting to be added');
 
-
-
 /*------------Programme----------------------*/
 /* the dataset focus on programmes of two departments: gynaecology and neurology
-Programme list:
+Programme list: gynaecology test, gynaecology diagnose, gynaecology therapy, gynaecology surgery, gynaecology recovery, gynaecology research,
+                neurology test, neurology diagnose, neurology therapy, neurology surgery, neurology recovery, neurology research
 */
-
-
+insert into "Programme" ("Id", "Name", "IsPublished") values (1, 'gynaecology test', true);
+insert into "Programme" ("Id", "Name", "IsPublished") values (2, 'gynaecology diagnose', true);
+insert into "Programme" ("Id", "Name", "IsPublished") values (3, 'gynaecology therapy', false);
+insert into "Programme" ("Id", "Name", "IsPublished") values (4, 'gynaecology surgery', false);
+insert into "Programme" ("Id", "Name", "IsPublished") values (5, 'gynaecology recovery', false);
+insert into "Programme" ("Id", "Name", "IsPublished") values (6, 'gynaecology research', true);
+insert into "Programme" ("Id", "Name", "IsPublished") values (7, 'neurology test', true);
+insert into "Programme" ("Id", "Name", "IsPublished") values (8, 'neurology diagnose', true);
+insert into "Programme" ("Id", "Name", "IsPublished") values (9, 'neurology therapy', false);
+insert into "Programme" ("Id", "Name", "IsPublished") values (10, 'neurology surgery', false);
+insert into "Programme" ("Id", "Name", "IsPublished") values (11, 'neurology recovery', true);
+insert into "Programme" ("Id", "Name", "IsPublished") values (12, 'neurology research', true);
 
 /*------------ActivityPermit----------------------*/
 /* the dataset focus on activities of two departments: gynaecology and neurology
